58329faa7d0cce46278bd370ac62064dd75b1541
jmhs11 <josemariahs@gmail.com>
2022-08-18 19:59:42 +0200

    More tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,219 +1,56 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useLazyQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+const query = gql`
+	query ($id: Int, $name: String) {
+		User(id: $id, name: $name) {
+			name
+			id
+			about(asHtml: true)
+		}
+	}
+`;
 
 const App = () => {
-	// const [data, setData] = useState();
-	// const [data1, setData1] = useState();
+	const [getUser, result] = useLazyQuery(query);
+	const [user, setUser] = useState();
 
-	// const handleChange = (ev, setData) => {
-	// 	setData(data);
-	// };
+	const showUser = () => {
+		getUser({
+			variables: { name: document.querySelector('[name="input"]').value }
+		});
+	};
 
-	const query = gql`
-		query ($id: Int, $name: String) {
-			User(id: $id, name: $name) {
-				id
-				name
-				previousNames {
-					name
-					updatedAt
-				}
-				avatar {
-					large
-				}
-				bannerImage
-				about
-				isFollowing
-				isFollower
-				donatorTier
-				donatorBadge
-				createdAt
-				moderatorRoles
-				isBlocked
-				bans
-				options {
-					profileColor
-					restrictMessagesToFollowing
-				}
-				mediaListOptions {
-					scoreFormat
-				}
-				statistics {
-					anime {
-						count
-						meanScore
-						standardDeviation
-						minutesWatched
-						episodesWatched
-						genrePreview: genres(limit: 10, sort: COUNT_DESC) {
-							genre
-							count
-						}
-					}
-					manga {
-						count
-						meanScore
-						standardDeviation
-						chaptersRead
-						volumesRead
-						genrePreview: genres(limit: 10, sort: COUNT_DESC) {
-							genre
-							count
-						}
-					}
-				}
-				stats {
-					activityHistory {
-						date
-						amount
-						level
-					}
-				}
-				favourites {
-					anime {
-						edges {
-							favouriteOrder
-							node {
-								id
-								type
-								status(version: 2)
-								format
-								isAdult
-								bannerImage
-								title {
-									userPreferred
-								}
-								coverImage {
-									large
-								}
-								startDate {
-									year
-								}
-							}
-						}
-					}
-					manga {
-						edges {
-							favouriteOrder
-							node {
-								id
-								type
-								status(version: 2)
-								format
-								isAdult
-								bannerImage
-								title {
-									userPreferred
-								}
-								coverImage {
-									large
-								}
-								startDate {
-									year
-								}
-							}
-						}
-					}
-					characters {
-						edges {
-							favouriteOrder
-							node {
-								id
-								name {
-									userPreferred
-								}
-								image {
-									large
-								}
-							}
-						}
-					}
-					staff {
-						edges {
-							favouriteOrder
-							node {
-								id
-								name {
-									userPreferred
-								}
-								image {
-									large
-								}
-							}
-						}
-					}
-					studios {
-						edges {
-							favouriteOrder
-							node {
-								id
-								name
-							}
-						}
-					}
-				}
-			}
-		}
-	`;
+	useEffect(() => {
+		setUser(result.data);
+	}, [result]);
 
-	const { loading, error, data } = useQuery(query, {
-		variables: { name: 'jmhs11' }
-	});
-	const {
-		loading: loading1,
-		error: error1,
-		data: data1
-	} = useQuery(query, {
-		variables: { name: 'ametralla' }
-	});
-
-	if (loading) return <p>Loading...</p>;
-	if (error) return <p>Error :(</p>;
-
-	if (loading1) return <p>Loading...</p>;
-	if (error1) return <p>Error :(</p>;
-
-	console.log(data);
-	console.log(data1);
 	return (
 		<>
-			{data.User.name}
-			<img src={data.User.avatar.large} />
-			{data1.User.name}
-			<img src={data1.User.avatar.large} />
+			<div className='flex'>
+				<div className='m-4'>
+					<input
+						className='block mb-4 input input-primary'
+						text='Button'
+						name='input'
+					/>
+				</div>
+				<div className='m-4'>
+					<input className='block mb-4 input input-primary' text='Button' />
+				</div>
+				<button type='button' onClick={showUser}>
+					Enviar
+				</button>
+			</div>
+			{user && (
+				<>
+					<p>{user.User.id}</p>
+					<p>{user.User.name}</p>
+					<p dangerouslySetInnerHTML={{ __html: user.User.about }}></p>
+				</>
+			)}
 		</>
 	);
-
-	// setData(result);
-
-	// return (
-	// 	<div className='flex'>
-	// 		{/* <div className='m-4'>
-	// 			<input
-	// 				className='input input-primary block mb-4'
-	// 				text='Button'
-	// 				onChange={ev => handleChange(ev, setData)}
-	// 			/>
-	// 			{data}
-	// 		</div>
-	// 		<div className='m-4'>
-	// 			<input
-	// 				className='input input-primary block mb-4'
-	// 				text='Button'
-	// 				onChange={ev => {
-	// 					const { loading, error, result } = useQuery(query, {
-	// 						variables: { name: 'jmhs11' }
-	// 					});
-
-	// 					if (loading) return <p>Loading...</p>;
-	// 					if (error) return <p>Error :(</p>;
-
-	// 					setData(result);
-	// 				}}
-	// 			/>
-	// 			{data1}
-	// 		</div> */}
-	// 	</div>
-	// );
 };
 
 export default App;
