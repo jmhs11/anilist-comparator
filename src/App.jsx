b27b6248cdd0e8faf874dded28bcf81f7595fd15
jmhs11 <josemariahs@gmail.com>
2022-08-23 11:24:21 +0200

    mantain desktop with 2 column layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import MediaSection from './components/MediaSection';
 const App = () => (
 	<div className='md:p-4'>
 		<Header />
-		{/* <main className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+		<main className='hidden md:grid grid-cols-1 md:grid-cols-2 gap-4'>
 			<MediaSection />
 			<MediaSection />
-		</main> */}
-		<Split direction='vertical' style={{ height: 'calc(100vh - 8rem)' }}>
+		</main>
+		<Split className='md:hidden' direction='vertical' style={{ height: 'calc(100vh - 8rem)' }}>
 			<MediaSection />
 			<MediaSection />
 		</Split>
