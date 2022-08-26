6598853a849bf6ad5020659dd2c8622192983101
jmhs11 <josemariahs@gmail.com>
2022-08-26 15:03:48 +0200

    Fix header text width in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ slot }) => {
 		<header className='my-2 md:my-4 px-4 flex justify-between items-center gap-4'>
 			<div className='flex justify-center items-center gap-4'>
 				<AniListIcon className='h-10 w-10' />
-				<h1 className='text-3xl font-bold text-white'>COMPARATOR</h1>
+				<h1 className='text-xl md:text-3xl font-bold text-white'>COMPARATOR</h1>
 			</div>
 			{slot}
 		</header>
